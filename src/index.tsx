bb46ba4920da5722baa7037c72b9edb0d2930221
matheuscorreiag <matheuscorreiags@gmail.com>
2021-12-03 12:28:43 -0300

    fix: react env variable

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './containers/App/App';
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
